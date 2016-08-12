cd6048fdcdcdb50d9d5507d7330b9473dcd0e1bb
Shane <shanebeehler@me.com>
2016-08-11 21:41:38 -0400

    Created instance of instructor and called greeting method.

--- people_assignment/people.rb
+++ people_assignment/people.rb
@@ -2,18 +2,26 @@ class Person
   def initialize(name)
     @name = name
   end
+
+  def greeting
+    "Hi, my name is #{@name}."
+  end
 end
 
 
 
-class Student
+class Student < Person
   def learn
     puts "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an object!"
   end
 end
+
+chris = Instructor.new("Chris")
+
+puts chris.greeting
