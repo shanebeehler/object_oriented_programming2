a92d5ec2abfef6633aec0cae26fc3aacf6bd96c1
Shane <shanebeehler@me.com>
2016-08-12 12:38:59 -0400

    Created imported and tax methods.

--- receipt_assignment/receipt.rb
+++ receipt_assignment/receipt.rb
@@ -1,5 +1,30 @@
 class Item
-  def initialize(item = gets.chomp, price = gets.chomp)
+  @@taxes_total
+  def initialize(item, price)
     @item = item
     @price = price
+    @price_with_tax = price
   end
+
+  def imported(@price)
+    import_tax = @price * 0.05
+    @@taxes_total += import_tax
+    @price_with_tax += import_tax
+  end
+
+  def sales_tax(@price)
+    sales_tax = @price * 0.1
+    @@taxes_total += sales_tax
+    @price_with_tax += sales_tax
+  end
+end
+
+class Receipt < Item
+  puts "1 #{@item} : "
+
+
+end
+
+book = Item.new("book", 12.49)
+cd = Item.new("music CD", 14.99)
+cb = Item.new("chocolate bar", 0.85)
